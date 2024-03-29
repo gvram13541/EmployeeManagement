31267b959b419be7813ad5215bef3c70824a81bd
Gunavardhan Reddy <guna955reddy@gmail.com>
2024-03-29 10:26:29 +0530

    Initial commit

---
+++ emp_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Role(models.Model):
+    name = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100)
+    dept = models.ForeignKey(Department, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    salary = models.FloatField(default=0)
+    bonus = models.FloatField(default=0)
+    phone = models.IntegerField(default=0)
+    hired_date = models.DateField()
+
+    def __str__(self):
+        return self.first_name
